b087010712fb45c2114bc6f3e887a3c7b1fed04d
kasperlundberg <kasperlundberg1@gmail.com>
2022-09-16 16:04:28 +0200

    Lagt till en Smurf klass, men enbart med en skelett i. Börja sketcha på funktioner

---
+++ main/java/edu/chalmers/projecttemplate/model/Smurf.java
@@ -0,0 +1,29 @@
+package main.java.edu.chalmers.projecttemplate.model;
+
+public class Smurf {
+
+    //Kanske bör vara i Board/Application(projectTemplate)
+    private boolean hasWon = false;
+
+    //(NE,E,SE,SW,W,NW), alla möjliga directions smurfen kan ha?
+    private int direction;
+    //Kanske bör ändras, med avseende på hur hexagonerna är implementerade
+    //Något sätt att indikera vilken knapp/hexagon man står på! :)
+    private int xPos, yPos;
+
+    public void moveSmurf(){
+
+    }
+
+    public void calculateRoute(){
+
+    }
+
+    public void jump(){
+
+    }
+
+
+
+
+}
